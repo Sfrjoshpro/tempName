fb625d7ca003da3ea5d0b4cd5ab52e95fe0cb5b9
Sfrjoshpro <sfrjoshpro@gmail.com>
2026-08-19 12:56:16 -0400

    add project intelligence package init

---
+++ engine/src/project_intel/__init__.py
@@ -0,0 +1,5 @@
+"""Language-neutral project intelligence with a Python-first adapter."""
+
+from .python_symbols import PythonSymbol, index_python_source
+
+__all__ = ["PythonSymbol", "index_python_source"]
